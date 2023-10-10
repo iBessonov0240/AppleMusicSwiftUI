fbe2a34b6fb98fad1e67137d78d18d31b36be55a
Ilia <ilya2491@gmail.com>
2023-10-10 14:35:56 +0700

    Initial Commit

---
+++ AppleMusicSwiftUI/App/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by i0240 on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusicSwiftUI/View/Modules/LibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by i0240 on 10.10.2023.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
